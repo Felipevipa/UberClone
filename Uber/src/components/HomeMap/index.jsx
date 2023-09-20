26bf63e20e5b8507f52fa8301be12a1ef72e0008
unknown <felipevipa@outlook.com>
2023-09-20 12:46:59 -0500

    fixed height for map

--- Uber/src/components/HomeMap/index.jsx
+++ Uber/src/components/HomeMap/index.jsx
@@ -5,27 +5,20 @@ import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 function HomeMap() {
     return (
-        <View style={{
-            height: 300,
-            backgroundColor: "#a0abff",
-            justifyContent: 'center',
-            alignItems: 'center',
-        }}>
-            {/* <Text>I am a map</Text> */}
-            <MapView
-                style={{
-                    height: '100%',
-                    width: '100%',
-                }}
-                provider={PROVIDER_GOOGLE}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421
-                }}
-            />
-        </View>
+
+        <MapView
+            style={{
+                height: '100%',
+                width: '100%',
+            }}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={{
+                latitude: 4.570868,
+                longitude: -74.297333,
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1,
+            }}
+        />
     )
 }
 
--- Uber/src/screens/SearchResults/index.jsx
+++ Uber/src/screens/SearchResults/index.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Dimensions, View } from 'react-native'
 import HomeMap from '../../components/HomeMap'
 import UberTypes from '../../components/UberTypes'
 
 
 function SearchResults() {
   return (
-    <View>
-      <HomeMap />
-
-      <UberTypes />
+    <View style={{
+      display: 'flex',
+      justifyContent: 'space-between',
+    }}>
+      <View style={{
+        height: Dimensions.get('window').height - 410
+      }}>
+        <HomeMap />
+      </View>
+      <View style={{
+        height: 410,
+      }}>
+        <UberTypes />
+      </View>
     </View>
   )
 }
