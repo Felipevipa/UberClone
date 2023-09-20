a599e76d26213880a78e999a1a92c2916bb01b82
unknown <felipevipa@outlook.com>
2023-09-20 12:49:54 -0500

    fixed height for home map

--- Uber/src/screens/HomeScreen/index.jsx
+++ Uber/src/screens/HomeScreen/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from "react-native"
+import { View, Text, Dimensions } from "react-native"
 
 import HomeMap from '../../components/HomeMap'
 import CovidMessage from '../../components/CovidMessage'
@@ -8,11 +8,15 @@ import HomeSearch from '../../components/HomeSearch'
 function HomeScreen() {
   return (
     <View>
+      <View style={{
+        height: Dimensions.get('window').height - 410
+      }}>
         <HomeMap />
-        {/* Covid Message */}
-        <CovidMessage />
-        {/* Bottom component */}
-        <HomeSearch />
+      </View>
+      {/* Covid Message */}
+      <CovidMessage />
+      {/* Bottom component */}
+      <HomeSearch />
     </View>
   )
 }
