c355dbe36fc39ff4fbacdfedb3100c297d84deab
unknown <felipevipa@outlook.com>
2023-09-20 13:13:04 -0500

    added cars markers on map

--- Uber/src/components/HomeMap/index.jsx
+++ Uber/src/components/HomeMap/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-import { View, Text } from "react-native"
+import { View, Text, Image, FlatList } from "react-native"
 
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
+
+import cars from '../../assets/data/cars'
 
 function HomeMap() {
+
+    const getImage = (type) => {
+        if (type === 'UberX') {
+            return require('../../assets/images/top-UberX.png')
+        }
+        if (type === 'Comfort') {
+            return require('../../assets/images/top-Comfort.png')
+        }
+        return require(`../../assets/images/top-UberXL.png`)
+    }
+
     return (
 
         <MapView
@@ -13,12 +26,33 @@ function HomeMap() {
             }}
             provider={PROVIDER_GOOGLE}
             initialRegion={{
-                latitude: 4.570868,
-                longitude: -74.297333,
-                latitudeDelta: 0.1,
-                longitudeDelta: 0.1,
-            }}
-        />
+                // latitude: 4.570868,
+                // longitude: -74.297333,
+                latitude: 28.450627,
+                longitude: -16.263045,
+                latitudeDelta: 0.0222,
+                longitudeDelta: 0.0121,
+            }}>
+            {cars.map((item) => (
+                <Marker
+                    key={cars.id}
+                    coordinate={{
+                        latitude: item.latitude,
+                        longitude: item.longitude,
+                    }}
+                // image={require('../../assets/images/top-UberX.png')}
+                >
+                    <Image
+                        style={{
+                            width: 70,
+                            height: 70,
+                            resizeMode: 'contain'
+                        }}
+                        source={getImage(item.type)} />
+                </Marker>
+            ))}
+
+        </MapView>
     )
 }
 
