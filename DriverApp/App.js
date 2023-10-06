be6eedd956e66a290a10156757078d711e20bdb0
unknown <felipevipa@outlook.com>
2023-10-06 10:07:15 -0500

    added vector icons

--- DriverApp/App.js
+++ DriverApp/App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Entypo from 'react-native-vector-icons/Entypo'
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
+      <Text><Entypo name={"user"} /></Text>
+
       <StatusBar style="auto" />
     </View>
   );
