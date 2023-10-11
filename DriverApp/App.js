260c3d75690e48de0feec86e205e73b888bf0da0
unknown <felipevipa@outlook.com>
2023-10-11 11:27:30 -0500

    added async storage library

--- DriverApp/App.js
+++ DriverApp/App.js
@@ -5,6 +5,10 @@ import { SafeAreaView } from 'react-native';
 import * as Location from 'expo-location';
 
 import HomeScreen from './src/screens/HomeScreen';
+import { Amplify } from 'aws-amplify';
+import config from './src/aws-exports'
+
+Amplify.configure(config);
 
 export default function App() {
 
